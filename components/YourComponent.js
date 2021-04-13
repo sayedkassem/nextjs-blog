eddd45f9f1a33919452b3aa8436eb8ed287eb61a
Sayed Kassem <sayedkassem-9@outlook.com>
2021-04-13 14:10:10 +0300

    first commit

---
+++ components/YourComponent.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+const YourComponent =() =>{
+    
+        <div>
+            <Image src="../public/images/profile.jpg"
+            height={144} width={144} alt="Sayed"/>
+        </div>
+}
